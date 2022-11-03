048ff390c18b3323646abbd4cd0bb98328830963
michaelhany510 <69060386+michaelhany510@users.noreply.github.com>
2022-11-03 12:48:14 +0200

    reel python link code

--- try.py
+++ try.py
@@ -1,163 +1,120 @@
+# from time import time
+# import numpy as np
+# from scipy import fftpack
+
+
+# def getFmaxMyCode(signalX,signalY):
+#     # print(np.round(sig,2))
+#     sig_fft = fftpack.fft(signalY)
+#     # print(sig_fft)
+#     amplitude = np.abs(sig_fft)
+#     print(np.round(amplitude,2))
+#     angle = np.angle(sig_fft)
+#     # print(angle)
+
+#     sampleFreq = fftpack.fftfreq(sig.size,d=timeStep)
+#     print(sampleFreq)
+
+#     amp_freq = np.array([amplitude,sampleFreq])
+#     ampPosition = amp_freq[0,:].argmax()
+#     peakFreq = amp_freq[1,ampPosition]
+#     return peakFreq
+
+# timeStep= 0.05
+
+# timeVec = np.arange(0,10,timeStep)
+# period = 5
+# sig = (np.sin(2*np.pi*timeVec/period) + 0.25*np.random.randn(timeVec.size))
+# #every 5*5 points it completes 1/4 a cycle
+# #every 5*20 point it completes a cycle
+
+# # print(np.round(sig,2))
+# sig_fft = fftpack.fft(sig)
+# # print(sig_fft)
+# amplitude = np.abs(sig_fft)
+# print(np.round(amplitude,2))
+# angle = np.angle(sig_fft)
+# # print(angle)
+
+# sampleFreq = fftpack.fftfreq(sig.size,d=timeStep)
+# print(sampleFreq)
+
+# amp_freq = np.array([amplitude,sampleFreq])
+# ampPosition = amp_freq[0,:].argmax()
+# peakFreq = amp_freq[1,ampPosition]
+# print(ampPosition)
+# print(peakFreq)
+
+# high_freq_fft = sig_fft.copy()
+# high_freq_fft[np.abs(sampleFreq) > peakFreq] = 0
+
+# print(high_freq_fft)
+
+# filteredSignal = fftpack.ifft(high_freq_fft)
+# print(np.abs(filteredSignal))
+
+
 import numpy as np
-from scipy import signal
-x = np.linspace(0, 10, 20, endpoint=False)
-y = np.cos(-x**2/6.0)
-f = signal.resample(y, 100)
-xnew = np.linspace(0, 10, 400, endpoint=False)
-import matplotlib.pyplot as plt
-plt.plot(x, y, 'go-', xnew, f, '.-', 10, y[0], 'ro')
-plt.legend(['data', 'resampled'], loc='best')
+from matplotlib import pyplot as plt
+
+SAMPLE_RATE = 44100  # Hertz
+DURATION = 5  # Seconds
+
+def generate_sine_wave(freq, sample_rate, duration):
+    x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    frequencies = x * freq
+    # 2pi because np.sin takes radians
+    y = np.sin((2 * np.pi) * frequencies)
+    return x, y
+
+# Generate a 2 hertz sine wave that lasts for 5 seconds
+x, y = generate_sine_wave(2, SAMPLE_RATE, DURATION)
+plt.plot(x, y)
 plt.show()
 
-def getTheYCoordinates(newX,signalX,signalY):
-    print('------------------------------')
-    y = []
-    for x_coordinate in newX:
-        for index in range(0,len(signalX)):
-            if x_coordinate < signalX[index]:
-                previousXIndex = index-1
-                followingXIndex = index
-                break
-        followingX = signalX[followingXIndex]
-        previousX = signalX[previousXIndex]
-        followingY = signalY[followingXIndex]
-        previousY = signalY[previousXIndex]
-        newYCoordinate = (x_coordinate-followingX)*(previousY - followingY)/(previousX - followingX)+(followingY)
-        y.append(newYCoordinate)
-    
-    return y        
+_, nice_tone = generate_sine_wave(400, SAMPLE_RATE, DURATION)
+_, noise_tone = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
+noise_tone = noise_tone * 0.3
 
-import numpy as np
-import matplotlib.pyplot as plt
-f1=300
-fs=800
-t_min =0
-t_max= 10/f1
-t= np.arange(t_min,t_max,.00001)
-x1=np.sin(2*np.pi*f1*t)
-Ts= 1/fs
-ts = np.arange(t_min,t_max,Ts)
-x1resampled=np.sin(2*np.pi*f1*ts)
-x1reconstructed=np.zeros(len(t))
-samples = len(ts)
-for i in range(1,len(t)):
-    for n in range(1,samples):
-        x1reconstructed[i]=x1reconstructed[i]+x1resampled[n]*np.sinc((t[i]-n*Ts)/Ts);
-plt.subplot(3,1,1)
-plt.plot(t,x1)
+mixed_tone = nice_tone + noise_tone
+normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767)
 
-plt.subplot(3,1,2)
-plt.scatter(ts,x1resampled)
+plt.plot(normalized_tone[:1000])
+plt.show()
+from scipy.io.wavfile import write
 
+# Remember SAMPLE_RATE = 44100 Hz is our playback rate
+write("mysinewave.wav", SAMPLE_RATE, normalized_tone)
 
-plt.subplot(3,1,3)
-plt.plot(t,x1reconstructed)
+from scipy.fft import rfft, rfftfreq
+
+# Number of samples in normalized_tone
+N = SAMPLE_RATE * DURATION
+
+yf = rfft(normalized_tone)
+xf = rfftfreq(N, 1 / SAMPLE_RATE)
+print(xf)
+
+plt.plot(xf, np.abs(yf))
 plt.show()
 
-# viewing the generated signals
-# for index, sgnal in st.session_state['signal'].items():
-#     if st.session_state['checkBoxes'][index]:
-#         st.write('Signal {}'.format(index))
-#         signalFigure, signalAxis = plt.subplots(1, 1)
-#         signalAxis.plot(sgnal[0], sgnal[1], linewidth=3)
-#         signalAxis.grid()
-#         st.plotly_chart(signalFigure, linewidth=3,use_container_width=True)
+# The maximum frequency is half the sample rate
+points_per_freq = len(xf) / (SAMPLE_RATE / 2)
 
-## change state every click
-# if st.sidebar.button('Noise'):
-#     if st.session_state['button_state']==True:
-#         st.session_state['noise'] = True
-#         st.session_state['button_state']=False
-#     else:
-#         st.session_state['noise'] = False
-#         st.session_state['button_state']=True
+# Our target frequency is 4000 Hz
+target_idx = int(points_per_freq * 4000)
+yf[target_idx - 1 : target_idx + 2] = 0
 
-# if st.sidebar.button('➖Delete noise'):
-#     st.session_state['noise'] = False
-# showing the summation of signals
-# changeableSignalFigure, changeableSignalAxis = plt.subplots(1, 1)
-# changeableSignalAxis.plot(st.session_state['sum'][0], st.session_state['sum'][1],color=color, linewidth=3)
-# changeableSignalAxis.grid()
-# st.plotly_chart(changeableSignalFigure,  linewidth=3)
+plt.plot(xf, np.abs(yf))
+plt.show()
 
-#---------------------------------------------------------------- sampling -------------------------------------------------------#
+from scipy.fft import irfft
 
-# import streamlit as st
-# import matplotlib.pyplot as plt
-# from scipy.interpolate import interp1d
-# import numpy as np  # np mean, np random ,np asarray, np 
+new_sig = irfft(yf)
 
-# col1, col2, col3 = st.columns(3)
-# with col2:
-#     st.title('Sampling')
+plt.plot(new_sig[:1000])
+plt.show()
 
-# def getYCoordinate(newPoint, signalAfterSampling, samplingPeriod,discreteTime):
-#     summation = 0
-#     for discreteTimePoint,correspondingSignalValue in zip(discreteTime, signalAfterSampling):
-#         summation = summation + correspondingSignalValue * np.sinc((1 / samplingPeriod) * (newPoint - discreteTimePoint ))
-#     return summation
+norm_new_sig = np.int16(new_sig * (32767 / new_sig.max()))
 
-# option = 0
-# if 'signal' in st.session_state:
-#     signals = []
-#     for index, sgnal in st.session_state['signal'].items():
-#         signals.append('Signal {}'.format(index))
-#     option = st.selectbox('Choose a signal to sample', signals)
-
-# if option:
-
-#     # get the index of the signal from the chosen string
-#     option = int(option[7:])
-
-#     selectedOptionFigure, selectedOptionAxis = plt.subplots(1, 1)
-#     analogSignal_time = st.session_state['signal'][option][0]
-#     analogSignalValue = st.session_state['signal'][option][1]
-    
-#     selectedOptionAxis.plot(analogSignal_time, analogSignalValue)
-#     selectedOptionAxis.grid()
-#     # st.plotly_chart(selectedOptionFigure)
-
-#     samplingFrequency = st.sidebar.slider('Sampling frequency (Hz)', 1, 100, 2)
-#     print(samplingFrequency)
-#     samplingPeriod = 1 / samplingFrequency
-    
-#     #the equivalent to line 53
-#     # discreteTimeUnNormalised = np.arange(analogSignal_time[0]/samplingPeriod, analogSignal_time[-1] / samplingPeriod)
-#     # discreteTime = discreteTimeUnNormalised * samplingPeriod
-    
-#     #the equivalent to the lines 49 and 50
-#     discreteTime = np.arange(analogSignal_time[0],analogSignal_time[-1],samplingPeriod)
-    
-    
-#     predict = interp1d(analogSignal_time, analogSignalValue, kind='quadratic')
-#     signalAfterSampling = np.array([predict(timePoint) for timePoint in discreteTime])
-
-#     interpolatedSignalFigure, interpolatedSignalAxis = plt.subplots(1, 1)
-
-#     # reconstructionTimeAxis = np.linspace(analogSignal_time[0], analogSignal_time[-1], 200,endpoint=False)
-#     #line 63 takes high processing time than 61 because it includes much more points to process
-#     reconstructionTimeAxis = analogSignal_time
-
-#     signalAfterReconstruction = np.array([getYCoordinate(timePoint, signalAfterSampling, samplingPeriod,discreteTime) for timePoint in reconstructionTimeAxis])
-#     selectedOptionAxis.plot(discreteTime, signalAfterSampling,'r.',reconstructionTimeAxis, signalAfterReconstruction, 'y--')
-    
-#     col1, col2 = st.columns(2)
-#     with col1:
-#         st.plotly_chart(selectedOptionFigure,use_container_width=True)
-    
-
-#     interpolatedSignalAxis.plot(reconstructionTimeAxis, signalAfterReconstruction, '-')
-#     with col2:
-#         st.plotly_chart(interpolatedSignalFigure,use_container_width=True)
-#     # st.write(signalAfterReconstruction)
-
-# else:
-#     st.write('Generate signals then choose one to sample')
-
-
-#-------------------------------------------- sampling----------------------------------------------------------------------#
-
-    # st.session_state['primaryKey'] = st.session_state['primaryKey'] + 1
-    # st.session_state['signal'][st.session_state['primaryKey']] = [analogSignalTime,analogSignalValue]
-    # st.session_state['uploaded'][st.session_state['primaryKey']] = True
-    
+write("clean.wav", SAMPLE_RATE, norm_new_sig)

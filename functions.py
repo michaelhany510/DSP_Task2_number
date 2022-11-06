9404428147f00fced199dc7b553610ad7907ad06
joeadham <yousefdhm@gmail.com>
2022-11-06 17:34:25 +0200

    Completed the uniform audio equalizer

--- functions.py
+++ functions.py
@@ -23,7 +23,7 @@ def getPeaksFrequencies(xAxis,yAxis):
 
 
 #-------------------------------------- Fourier Transform on Audio ----------------------------------------------------
-def audio_fourier_transform(audio_file,guitar,flute,biano,spectroCheckBox):
+def audio_fourier_transform(audio_file,guitar,flute,piano,spectroCheckBox):
     column1,column2 = st.columns(2)
     with column1:    
         st.audio(audio_file, format='audio/wav') # displaying the audio
@@ -57,7 +57,7 @@ def audio_fourier_transform(audio_file,guitar,flute,biano,spectroCheckBox):
     # these three lines determine the range that will be modified by the slider
     yf[:int(600*points_per_freq)] *= guitar
     yf[int(600*points_per_freq):int(3000*points_per_freq)] *= flute
-    yf[int(3000*points_per_freq):] *= biano
+    yf[int(3000*points_per_freq):] *= piano
     
     
     
@@ -75,6 +75,66 @@ def audio_fourier_transform(audio_file,guitar,flute,biano,spectroCheckBox):
     # with column2:
     #     plotting(xf,np.abs(yf))
     
+def uniform_audio_fourier_transform(audio_file,comp_1,comp_2,comp_3,comp_4,comp_5,comp_6,comp_7,comp_8,comp_9,comp_10,spectroCheckBox):
+    column1,column2 = st.columns(2)
+    with column1:    
+        st.audio(audio_file, format='audio/wav') # displaying the audio
+    obj = wave.open(audio_file, 'rb')
+    sample_rate = obj.getframerate()      # number of samples per second
+    n_samples   = obj.getnframes()        # total number of samples in the whole audio
+    duration    = n_samples / sample_rate # duration of the audio file
+    signal_wave = obj.readframes(-1)      # amplitude of the sound
+
+    
+    signal_y_axis = np.frombuffer(signal_wave, dtype=np.int32)
+    signal_x_axis = np.linspace(0, duration, len(signal_y_axis))
+    with column1:
+        if not spectroCheckBox:
+            plotting(signal_x_axis[:1000],signal_y_axis[:1000])
+        else:
+            plot_spectro(audio_file.name)
+    
+    
+    yf = rfft(signal_y_axis) # returns complex numbers of the y axis in the data frame
+    xf = rfftfreq(len(signal_y_axis), (signal_x_axis[1]-signal_x_axis[0])) # returns the frequency x axis after fourier transform
+
+    peaks = find_peaks(yf)   # computes peaks of the signal 
+    peaks_indeces = peaks[0] # indeces of frequency with high peaks
+    points_per_freq = len(xf) / (xf[-1]) # NOT UNDERSTANDABLE 
+    
+    # with column1:
+    #     plotting(xf,np.abs(yf))
+
+
+    # these three lines determine the range that will be modified by the slider
+    yf[int(20*points_per_freq):int(2000*points_per_freq)] *= comp_1
+    yf[int(2000*points_per_freq):int(4000*points_per_freq)] *= comp_2
+    yf[int(4000*points_per_freq):int(6000*points_per_freq)] *= comp_3
+    yf[int(6000*points_per_freq):int(8000*points_per_freq)] *= comp_4
+    yf[int(8000*points_per_freq):int(10000*points_per_freq)] *= comp_5
+    yf[int(10000*points_per_freq):int(12000*points_per_freq)] *= comp_6
+    yf[int(12000*points_per_freq):int(14000*points_per_freq)] *= comp_7
+    yf[int(14000*points_per_freq):int(16000*points_per_freq)] *= comp_8
+    yf[int(16000*points_per_freq):int(18000*points_per_freq)] *= comp_9
+    yf[int(18000*points_per_freq):int(20000*points_per_freq)] *= comp_10
+    
+    
+    
+    
+    modified_signal = irfft(yf) # returning the inverse transform after modifying it with sliders 
+    tryyy = np.int32(modified_signal)
+
+    
+    write("example.wav", sample_rate, tryyy)
+    with column2:
+        st.audio("example.wav", format='audio/wav')
+        if not spectroCheckBox:
+            plotting(signal_x_axis[:1000],tryyy[:1000])
+        else:
+            plot_spectro("example.wav")
+    # with column2:
+    #     plotting(xf,np.abs(yf))
+   
 def plotting(x_axis_fourier,fft_out):
     # Plotting audio Signal
     figure, axis = plt.subplots()

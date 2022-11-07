4e22476168641d80299997c10597731cbaa3845c
EHVB <ehab.mahmoud00@eng-st.cu.edu.eg>
2022-11-07 17:41:50 +0200

    vowels still not working !

--- ehab.py
+++ ehab.py
@@ -30,10 +30,26 @@ plt.plot(xf, np.abs(yf))
 st.plotly_chart(fig, use_container_width=True)
 
 points_per_freq = len(xf) / (sample_rate / 2)
-
+st.write(points_per_freq)
 # Our target frequency is 4000 Hz
-target_idx1 = int(points_per_freq * 0)
-target_idx2 = int(points_per_freq * 1000)
+target_idx1 = int(points_per_freq * 27)
+target_idx2 = int(points_per_freq * 227)
+yf[target_idx1: target_idx2] *= 0
+
+target_idx1 = int(points_per_freq * 480)
+target_idx2 = int(points_per_freq * 680)
+yf[target_idx1: target_idx2] *= 0
+
+target_idx1 = int(points_per_freq * 1699)
+target_idx2 = int(points_per_freq * 1899)
+yf[target_idx1: target_idx2] *= 0
+
+target_idx1 = int(points_per_freq * 2505)
+target_idx2 = int(points_per_freq * 2705)
+yf[target_idx1: target_idx2] *= 0
+
+target_idx1 = int(points_per_freq * 3577)
+target_idx2 = int(points_per_freq * 3777)
 yf[target_idx1: target_idx2] *= 0
 
 fig = plt.figure()

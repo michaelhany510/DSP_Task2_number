9786e2701981c7db850272e0c08c53e31ff79d77
EHVB <ehab.mahmoud00@eng-st.cu.edu.eg>
2022-11-14 21:46:30 +0200

    vowels still not working yet !!!

--- functions.py
+++ functions.py
@@ -196,13 +196,11 @@ def vowel_audio_fourier_transform(file, er_vowel, a_vowel, iy_vowel, oo_vowel, u
     with column1:
         st.audio(file, format='audio/wav')
 
-    obj = wave.open(file, 'rb')
-    sample_rate = obj.getframerate()      # number of samples per second
-    n_samples = obj.getnframes()        # total number of samples in the whole audio
+    tone,sample_rate = sf.read(file)
+    n_samples = tone.shape[0]     # total number of samples in the whole audio
     duration = n_samples / sample_rate  # duration of the audio file
-    signal_wave = obj.readframes(-1)      # amplitude of the sound
 
-    signal_y_axis = np.frombuffer(signal_wave, dtype=np.int16)
+    signal_y_axis = tone
     signal_x_axis = np.linspace(0, duration, len(signal_y_axis))
 
     with column1:
@@ -212,9 +210,9 @@ def vowel_audio_fourier_transform(file, er_vowel, a_vowel, iy_vowel, oo_vowel, u
             plot_spectro(file)
 
     yf = rfft(signal_y_axis)
-    xf = rfftfreq(len(signal_y_axis), (signal_x_axis[1]-signal_x_axis[0]))
+    xf = rfftfreq(len(signal_y_axis), 1/sample_rate)
 
-    points_per_freq = len(xf) / (xf[-1])
+    points_per_freq = len(xf) / (sample_rate/2)
     # er vowel frequencies
     yf[int(440*points_per_freq):int(540*points_per_freq)
        ] = vowel_triang_window(yf, 440, 540, er_vowel, points_per_freq)
@@ -251,14 +249,13 @@ def vowel_audio_fourier_transform(file, er_vowel, a_vowel, iy_vowel, oo_vowel, u
     yf[int(2340*points_per_freq):int(2440*points_per_freq)
        ] = vowel_triang_window(yf, 2340, 2440, uh_vowel, points_per_freq)
     modified_signal = irfft(yf)
-    norm_modified = np.int16(modified_signal)
 
-    write("vowel_modified.wav", sample_rate, norm_modified)
+    sf.write("vowel_modified.wav",modified_signal ,sample_rate)
 
     with column2:
         st.audio("vowel_modified.wav", format='audio/wav')
         if not spectroCheckBox:
-            plotting(signal_x_axis[:1000], norm_modified[:1000])
+            plotting(signal_x_axis[:1000], modified_signal[:1000])
         else:
             plot_spectro("vowel_modified.wav")
 
